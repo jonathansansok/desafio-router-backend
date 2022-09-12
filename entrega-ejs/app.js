7bb11156d2306b456f9d814782d35ab9df8fcbaa
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-09-11 23:58:59 -0300

    js

--- entrega-ejs/app.js
+++ entrega-ejs/app.js
@@ -4,20 +4,30 @@ const app = express();
 const server = app.listen(8080, () => console.log("server up ejs"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-let users = []
+let productos = [];
 
 app.get("/", (req, res) => {
-  res.render('home', {
-    users: users
-  }) 
-  
-})
-app.post('/users', (req, res) => {
-  users.push(req.body)
-  res.redirect('/') 
-})
+  res.render("home", {
+    productos: productos,
+  });
+});
+app.post("/", (req, res) => {
+  productos.push(req.body);
+  res.redirect("/");
+});
+
+app.get("/productos", (req, res) => {
+  res.render("home", {
+    productos: productos,
+  });
+});
+
+app.post("/productos", (req, res) => {
+  productos.push(req.body);
+  res.redirect("/");
+});
